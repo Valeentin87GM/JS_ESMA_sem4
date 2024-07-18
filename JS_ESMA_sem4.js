0d8d4f69a6e835982313e562c5d11aa628f2c689
Valeentin87GM <valeentin87@list.ru>
2024-07-19 00:09:36 +0300

    успешное решение задачи № 1 с использованием fetch

--- JS_ESMA_sem4.js
+++ JS_ESMA_sem4.js
@@ -11,44 +11,68 @@
 // Если запрос неуспешен, функция отклоняет промис с сообщением об ошибке.
 
 
-/* не получилось использовать fetch............
 
-fetch('./usersData.json')
+
+let result = fetch('./usersData.json')
   .then(response => {
     if (!response.ok) {
       throw new Error('Ой, ошибка в fetch: ' + response.statusText);
     }
     return response.json();
   })
-  .then(jsonData => console.log(jsonData))
+  .then(jsonData => getUserData(jsonData,5))
   .catch(error => console.error('Ошибка при исполнении запроса: ', error));
-
-*/
-
-
-const usersData = '{ "15":{"id":1,"name":"Иван","age":25}, "2":{"id":2,"name":"Пётр","age":35}, "3":{"id":3,"name":"Олег","age":20}}';
-
 
 
 const getUserData = function(data,idUser){
-    const dataParse = JSON.parse(data);
     
-    Object.values(dataParse).forEach(user => {
+    
+    Object.values(data).forEach(user => {
         result = `Пользователь с  id = ${idUser} не числится`
-        console.log(user.id);
+        
         if (user.id == idUser) {
             console.log('Совпадение данных');
             result = user;
         }       
     });
+    console.log(result);
     return result
+
 }
 
 
 
-const searchUser = getUserData(usersData, 1);
+// const searchUser = getUserData(result, 1);
 
-console.log(searchUser);
+// console.log(searchUser);
+
+
+
+// ""Отправка данных на сервер""
+
+// Реализуйте функцию saveUserData, которая принимает объект с данными о пользователе в качестве аргумента и использует fetch 
+// для отправки этих данных на удаленный сервер для сохранения. Функция должна возвращать промис, который разрешается, если данные успешно отправлены, или отклоняется в случае ошибки.
+
+// *Подсказка *
+// // Пример использования функции
+// const user = {
+// name: 'John Smith',
+// age: 30,
+// email: 'john@example.com'
+// };
+
+// saveUserData(user)
+// .then(() => {
+// console.log('User data saved successfully');
+// })
+// .catch(error => {
+// console.log(error.message);
+// });
+
+// saveUserData использует fetch для отправки данных о пользователе на удаленный сервер для сохранения. 
+// Она отправляет POST-запрос на URL-адрес /users с указанием типа содержимого application/json и сериализует объект с данными 
+// о пользователе в JSON-строку с помощью JSON.stringify(). Если запрос успешен (с кодом 200), функция разрешает промис. 
+// Если запрос неуспешен, функция отклоняет промис с сообщением
 
 
 
